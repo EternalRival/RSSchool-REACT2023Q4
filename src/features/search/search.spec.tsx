7b0b4ac0a81f79d88515c46fd357c80d5b8ed747
EternalRival <eternalrival8@gmail.com>
2023-11-19 07:32:43 +0300

    test: transfer search tests

--- src/features/search/search.spec.tsx
+++ src/features/search/search.spec.tsx
@@ -1,33 +1,22 @@
-import { RenderResult, cleanup, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { searchQueryLocalStorageKey } from 'shared/constants';
-import { renderWithRouter } from 'tests/test-utils';
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { MemoryRouterWithStore } from 'tests/test-utils';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import { Search } from '.';
 
 describe('Search', () => {
   const user = userEvent.setup();
-  const renderSearch = (): RenderResult => {
-    return renderWithRouter(<Search />);
-  };
-
   const storageGetSpy = vi.spyOn(Storage.prototype, 'getItem');
   const storageSetSpy = vi.spyOn(Storage.prototype, 'setItem');
 
-  beforeEach(() => {
-    renderSearch();
-  });
-
   afterEach(() => {
     localStorage.clear();
-    cleanup();
-    vi.clearAllMocks();
   });
 
   it('Verify that clicking the Search button saves the entered value to the local storage', async () => {
+    render(<MemoryRouterWithStore element={<Search />} />);
     const searchValue = 'doctor';
-
-    localStorage.setItem(searchQueryLocalStorageKey, '');
     expect(localStorage.getItem(searchQueryLocalStorageKey)).not.toBe(
       searchValue
     );
@@ -44,11 +33,9 @@ describe('Search', () => {
   it('Check that the component retrieves the value from the local storage upon mounting', async () => {
     const searchValue = 'dark';
 
-    cleanup();
-
     localStorage.setItem(searchQueryLocalStorageKey, searchValue);
 
-    renderSearch();
+    render(<MemoryRouterWithStore element={<Search />} />);
 
     expect(storageGetSpy).lastReturnedWith(searchValue);
     expect(screen.getByRole('searchbox')).toHaveValue(searchValue);
