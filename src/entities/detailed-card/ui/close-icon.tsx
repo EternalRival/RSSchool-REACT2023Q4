ecda9eec1b0664c9f1d2fa2bee8d24472e60be21
EternalRival <eternalrival8@gmail.com>
2023-11-24 22:57:25 +0300

    feat: restore detailed card

---
+++ src/entities/detailed-card/ui/close-icon.tsx
@@ -0,0 +1,13 @@
+import { FC, SVGProps } from 'react'
+
+export const CloseIcon: FC<SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    viewBox="0 -960 960 960"
+    width="24"
+    {...props}
+  >
+    <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+  </svg>
+)
--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,16 +1,23 @@
+import { DetailedCard } from '@entities/detailed-card'
 import { ErrorThrowingButton } from '@features/error-throwing-button'
 import { BottomSection } from '@widgets/bottom-section'
 import { TopSection } from '@widgets/top-section'
 import { FC } from 'react'
+import { mockDetails } from '../../tests/mock/mock-details-response'
 
 export const MainPage: FC = () => {
   return (
-    <main className="flex flex-col justify-between">
-      <ErrorThrowingButton />
-      <h1 hidden={true}>TV Shows App</h1>
-      <TopSection />
-      <BottomSection />
-    </main>
-    // <Outlet />
+    <>
+      <main className="flex flex-col justify-between">
+        <ErrorThrowingButton />
+        <h1 hidden={true}>TV Shows App</h1>
+        <TopSection />
+        <BottomSection />
+      </main>
+      <DetailedCard
+        handleClose={() => {}}
+        {...mockDetails}
+      />
+    </>
   )
 }
